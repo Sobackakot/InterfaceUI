b584b6ecdd618e01ba173cb6c930ba8f32b95662
Aibulat <Myyfeedi@gmail.com>
2023-09-30 00:37:35 +0600

    Save

---
+++ Assets/Skripts/UIManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject MenuUI;
+     
+    [SerializeField] private GameObject defaultImageObj;
+    [SerializeField] private GameObject activImageObj;
+
+    [SerializeField] private List<GameObject> defaultObjects;
+    [SerializeField] private List<GameObject> activeObjects;
+
+    private void Start()
+    {
+        ReturnImageDefault();
+    }
+
+    public void OpenMenuButton()
+    {
+        CloseMenuButton();
+        defaultImageObj.gameObject.SetActive(false); 
+        activImageObj.gameObject.SetActive(true);
+        MenuUI.gameObject.SetActive(true);
+    }
+
+    public void CloseMenuButton()
+    { 
+        MenuUI.gameObject.SetActive(false);
+        foreach(GameObject go in activeObjects)
+        {
+            go.SetActive(false);
+        }
+        foreach (GameObject go in defaultObjects)
+        {
+            go.SetActive(true);
+        }
+    }
+
+    private void ReturnImageDefault()
+    { 
+        defaultImageObj.gameObject.SetActive(true); 
+        activImageObj.gameObject.SetActive(false);
+    }
+}
